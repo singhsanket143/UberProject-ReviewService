67b4362ece6280c8d45658daf95e2f76f220a9c2
Sanket Singh <singhsanket143@gmail.com>
2024-04-12 21:02:44 +0530

    basic spring data jpa config done

---
+++ src/main/java/com/example/uberreviewservice/UberReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberReviewServiceApplication.class, args);
+    }
+
+}
