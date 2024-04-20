0ff3ca8d69c5e66e8bd2694efdfe9b03be06a0df
PranotiPD <pranotidurugkar1111@gmail.com>
2024-04-20 19:33:04 +0530

    Review controller and service layer

--- src/main/java/com/example/uberreviewservice/services/ReviewService.java
+++ src/main/java/com/example/uberreviewservice/services/ReviewService.java
@@ -7,9 +7,13 @@ import java.util.Optional;
 
 public interface ReviewService {
 
+    public Review createReview(Review review);
+
     public Optional<Review> findReviewById(Long id);
 
     public List<Review> finalAllReviews();
+
+    public Review updateReview(Review review, Long id);
 
     public boolean deleteReviewById(Long id);
 
